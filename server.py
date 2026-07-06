d6f2e57eef7b493503ed0f840687ef447baf4978
Jason Bian <jasonzb@umich.edu>
2026-07-05 20:46:37 -0400

    CORS headers + OPTIONS preflight for browser frontends

--- server.py
+++ server.py
@@ -125,8 +125,18 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(code)
         self.send_header("Content-Type", "application/json")
         self.send_header("Content-Length", str(len(body)))
+        self.send_header("Access-Control-Allow-Origin", "*")
         self.end_headers()
         self.wfile.write(body)
+
+    def do_OPTIONS(self):
+        # CORS preflight for browser frontends; auth still enforced on the real request
+        self.send_response(204)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "Authorization, Content-Type")
+        self.send_header("Access-Control-Max-Age", "86400")
+        self.end_headers()
 
     def auth(self):
         header = self.headers.get("Authorization", "")
